1389997f3e857362d41b1617424b38cffa1bb316
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-15 10:08:01 +0000

    Included answer in repository

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import numpy as np
 nspins = 10 
 
 allup = np.zeros(nspins)
+for i in range(nspins) : allup[i]=1
 
 alldown = np.zeros(nspins)
+for i in range(nspins) : alldown[i]=-1
 
 alternating = np.zeros(nspins)
+for i in range(nspins) : 
+  if i%2==0 : alternating[i]=-1
+  else : alternating[i]=1
